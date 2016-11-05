c1e2e26872cac4d35d30c9fa4787c7693edeaf9a
Rub21 <ruben@mapbox.com>
2016-11-05 07:18:51 -0200

    update with session

--- index.js
+++ index.js
@@ -1,42 +1,73 @@
+var Hapi = require('hapi');
+var yar = require('yar');
 
-var Hapi = require('hapi')
-  , yar = require('yar')
-
-var Grant = require('grant-hapi')
-  , grant = new Grant()
+var Grant = require('grant-hapi');
+var grant = new Grant();
 
 var server = new Hapi.Server()
-server.connection({host: 'localhost', port: 3000})
+server.connection({
+  host: 'localhost',
+  port: 3000
+});
 
-server.route({method: 'GET', path: '/', handler: function (req, res) {
-  res({status:'ok'});
-}})
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function(req, res) {
+    res({
+      status: 'ok'
+    });
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/handle_openstreetmap_callback',
+  handler: function(req, res) {
+    var response = (req.session || req.yar).get('grant').response
+    console.log(response)
+    res(JSON.stringify(response, null, 2))
+  }
+});
 
 
-server.route({method: 'GET', path: '/user', handler: function (req, res) {
-  res({status:'ok'});
-}})
-
-
-server.route({method: 'GET', path: '/handle_openstreetmap_callback', handler: function (req, res) {
-  console.log(req.query)
-  res(JSON.stringify(req.query, null, 2))
-}})
-
-server.register([{
-  register: grant,
-  options: require('./config.json')
-}, {
-  register: yar,
-  options: {
-    cookieOptions: {
-      password: '214343pass214343passwordsaqwjnurdeqwwordsaqwjnurdeqw',
-      isSecure: false
+server.route({
+  method: 'GET',
+  path: '/status',
+  handler: function(req, res) {
+    var response = (req.session || req.yar).get('grant').response;
+    if (response) {
+      res(JSON.stringify(response, null, 2));
+    } else {
+      res({
+        status: 'bad'
+      });
     }
   }
-}], function (err) {
-  if (err) throw err
+});
+
+server.register([
+  {
+    register: yar,
+    options: {
+      maxCookieSize: 0,
+      cache: {
+        expiresIn: 24 * 60 * 60 * 1000
+      },
+      cookieOptions: {
+        password: 'abcdefghigklmnopqrsdasdasdadadadsdtuvwxyz123456', // min 32 chars
+        isSecure: false
+      }
+    }
+  }, {
+    register: grant,
+    options: require('./config.json')
+  }
+], function(err) {
+  if (err) {
+    throw err
+  }
   server.start(function() {
     console.log(`Server running at: ${server.info.uri}`);
-  })
-})
+  });
+});
